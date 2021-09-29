2f2f8260fe7b84532a1c5d3d3cb9c3b0e4c91a9a
zmousavi <zmousavi@users.noreply.github.com>
2021-09-28 23:09:27 -0400

    Update venv.sh

--- venv.sh
+++ venv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# python3 -m venv env
-# source env/bin/activate
-# pip install pyyaml
-# pip install pandas
-# pip install scipy
-# pip install matplotlib
-# pip install matplotlib_venn
-# pip install graphviz
+python3 -m venv env
+source env/bin/activate
+pip install pyyaml
+pip install pandas
+pip install scipy
+pip install matplotlib
+pip install matplotlib_venn
+pip install graphviz
