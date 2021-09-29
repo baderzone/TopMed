6c7e611dd0e40071526e4e078d82cfe2f0664ef8
zmousavi <zmousavi@users.noreply.github.com>
2021-09-28 22:49:26 -0400

    Add files via upload

---
+++ venv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# python3 -m venv env
+# source env/bin/activate
+# pip install pyyaml
+# pip install pandas
+# pip install scipy
+# pip install matplotlib
+# pip install matplotlib_venn
+# pip install graphviz
